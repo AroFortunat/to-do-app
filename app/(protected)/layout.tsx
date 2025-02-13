f6ed2c9415c881cd479df51725020c8deba2b898
Aro Fortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-02-13 16:52:04 +0300

    fix : navbar and add : logo

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,21 +1,30 @@
 'use client'
 import { SignedIn, UserButton, useUser } from '@clerk/nextjs'
-import Link from 'next/link'
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
 interface propss {
     children: ReactNode
 }
 const layout: React.FC<propss> = ({ children }) => {
-    const {user} = useUser()
+    const { user } = useUser()
     return (
         <div>
             <nav className='flex items-center justify-between p-4'>
-                <div>Logo</div>
+                <div>
+                    <a href={`/task/${user?.id}`}>
+                        <Image
+                            className='ml-5 w-44'
+                            alt='image du logo'
+                            width={741}
+                            height={286}
+                            src={'/Logo.png'} />
+                    </a>
+                </div>
                 <div className=''>
                     <ul className='flex gap-4'>
-                        <li className='btn btn-secondary text-white'><Link href={`/task/${user?.id}`}>Task</Link></li>
-                        <li className='btn btn-secondary text-white'><Link href={'/new-task'}>New Task</Link></li>
-                        <li className='btn btn-secondary text-white'><Link href={`/task-status`}>Task Status</Link></li>
+                        <li className='btn btn-secondary text-white'><a href={`/task/${user?.id}`}>Task</a></li>
+                        <li className='btn btn-secondary text-white'><a href={'/new-task'}>New Task</a></li>
+                        <li className='btn btn-secondary text-white'><a href={`/task-status`}>Task Status</a></li>
                     </ul>
                 </div>
                 <div className='mr-6'>
--- app/(protected)/new-task/page.tsx
+++ app/(protected)/new-task/page.tsx
@@ -1,9 +1,99 @@
-import React from 'react'
+'use client'
+import React, { ReactEventHandler } from 'react'
 
 const page = () => {
+  const handleSubmit:ReactEventHandler = (e)=>{
+    e.preventDefault()
+  }
   return (
-    <div>
-      
+    <div className='h-screen'>
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-lg">
+          <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">Get started today Add your New Task</h1>
+
+          <p className="mx-auto mt-4 max-w-md text-center italic text-gray-500">
+            "Petit à petit, l'œuvre prend vie !"
+            N’oubliez pas de célébrer chaque étape accomplie. 💪
+          </p>
+
+          <form onSubmit={handleSubmit} className="mt-6 mb-0 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
+            <h1 className="text-center text-lg font-bold">👏 Add your New Task</h1>
+
+            <div>
+              <label htmlFor="Titre" className="font-semibold">Title</label>
+              <div className="relative">
+                <input
+                  required
+                  id='Titre'
+                  name='title'
+                  type="text"
+                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs"
+                  placeholder="Enter Title for your new Task"
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="desc" className="font-semibold">Description</label>
+              <div className="relative">
+                <textarea className="textarea textarea-bordered w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs" placeholder="Entrer votre description"></textarea>
+
+              </div>
+            </div>
+            <div>
+              <label htmlFor="selection" className="font-semibold">Assign At :</label>
+              <div>
+                <select
+                  name="select"
+                  id="selection"
+                  className="mt-1.5 w-full p-2 rounded-lg border-gray-300 text-gray-700 sm:text-sm"
+                >
+                  <option value="">Please select</option>
+                  <optgroup label="A">
+                    <option value="AK">Albert King</option>
+                  </optgroup>
+
+                  <optgroup label="B">
+                    <option value="BBK">B.B King</option>
+                    <option value="BG">Buddy Guy</option>
+                  </optgroup>
+
+                  <optgroup label="E">
+                    <option value="EC">Eric Clapton</option>
+                  </optgroup>
+
+                  <optgroup label="J">
+                    <option value="JM">John Mayer</option>
+                    <option value="JH">Jimi Hendrix</option>
+                  </optgroup>
+
+                  <optgroup label="S">
+                    <option value="SRV">Stevie Ray Vaughn</option>
+                  </optgroup>
+                </select>
+              </div>
+
+            </div>
+            <div>
+              <label htmlFor="email" className="font-semibold">Deadline : </label>
+              <div className="relative">
+                <input
+                  type="date"
+                  className="w-full rounded-lg border-gray-500 p-4 pe-12 text-sm shadow-xs"
+                  placeholder="Enter Title for your new Task"
+                />
+              </div>
+            </div>
+
+
+            <button
+              type="submit"
+              className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+            >
+              Enregistrer
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
