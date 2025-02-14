cff111016efbdfb4b96753417c01e76ea460600a
Aro Fortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-02-14 11:17:52 +0300

    fix : ui task list

--- app/(protected)/task/components/UrgentAndImportant.tsx
+++ app/(protected)/task/components/UrgentAndImportant.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const UrgentAndImportant = () => {
     return (
         <div className='max-h-screen h-1/2'>
-            <article className="rounded-xl border border-gray-700 bg-gray-800 p-4">
-                <div className="flex items-center gap-4">
+            <article className="rounded-xl border border-red-500 ">
+                <div className="flex bg-red-500 items-center p-5 rounded-t-xl gap-4">
                     <div className='w-10 h-10 border-2 flex items-center justify-center rounded-full'>
                         <span className='text-white font-bold'>1</span>
                     </div>                    <div>
@@ -14,12 +14,12 @@ const UrgentAndImportant = () => {
                     </div>
                 </div>
 
-                <ul className="mt-4 space-y-2">
+                <ul className="mt-4 p-4 space-y-2">
                     <li>
                         <a href="#" className="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600">
-                            <strong className="font-medium text-white">Project A</strong>
+                            <strong className="font-medium">Project A</strong>
 
-                            <p className="mt-1 text-xs font-medium text-gray-300">
+                            <p className="mt-1 text-xs font-medium">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consequuntur deleniti,
                                 unde ab ut in!
                             </p>
@@ -28,9 +28,9 @@ const UrgentAndImportant = () => {
 
                     <li>
                         <a href="#" className="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600">
-                            <strong className="font-medium text-white">Project B</strong>
+                            <strong className="font-medium">Project B</strong>
 
-                            <p className="mt-1 text-xs font-medium text-gray-300">
+                            <p className="mt-1 text-xs font-medium">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cumque saepe sit.
                             </p>
                         </a>
--- app/(protected)/task/components/UrgentNotImportant.tsx
+++ app/(protected)/task/components/UrgentNotImportant.tsx
@@ -3,39 +3,22 @@ import React from 'react'
 const UrgentNotImportant = () => {
     return (
         <div className='max-h-screen h-1/2'>
-            <article className="rounded-xl border border-gray-700 bg-gray-800 p-4">
-                <div className="flex items-center gap-4">
+            <article className="rounded-xl border border-blue-600 ">
+                <div className="flex bg-blue-600 items-center p-5 rounded-t-xl gap-4">
                     <div className='w-10 h-10 border-2 flex items-center justify-center rounded-full'>
                         <span className='text-white font-bold'>2</span>
-                    </div>
+                    </div>                    <div>
+                        <h3 className="text-lg font-medium text-white">Urgent and not important</h3>
 
-                    <div>
-                        <h3 className="text-lg font-medium text-white">Urgent Not Important</h3>
-
-                        <div className="flow-root">
-                            <ul className="-m-1 flex flex-wrap">
-                                <li className="p-1 leading-none">
-                                    <a href="#" className="text-xs font-medium text-gray-300"> Twitter </a>
-                                </li>
-
-                                <li className="p-1 leading-none">
-                                    <a href="#" className="text-xs font-medium text-gray-300"> GitHub </a>
-                                </li>
-
-                                <li className="p-1 leading-none">
-                                    <a href="#" className="text-xs font-medium text-gray-300">Website</a>
-                                </li>
-                            </ul>
-                        </div>
                     </div>
                 </div>
 
-                <ul className="mt-4 space-y-2">
+                <ul className="mt-4 p-4 space-y-2">
                     <li>
                         <a href="#" className="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600">
-                            <strong className="font-medium text-white">Project A</strong>
+                            <strong className="font-medium">Project A</strong>
 
-                            <p className="mt-1 text-xs font-medium text-gray-300">
+                            <p className="mt-1 text-xs font-medium">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime consequuntur deleniti,
                                 unde ab ut in!
                             </p>
@@ -44,9 +27,9 @@ const UrgentNotImportant = () => {
 
                     <li>
                         <a href="#" className="block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600">
-                            <strong className="font-medium text-white">Project B</strong>
+                            <strong className="font-medium">Project B</strong>
 
-                            <p className="mt-1 text-xs font-medium text-gray-300">
+                            <p className="mt-1 text-xs font-medium">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente cumque saepe sit.
                             </p>
                         </a>
