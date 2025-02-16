f9156c7781370730c8c60fcfa1f1e29b3aaa303c
Aro Fortunat <aro.fortunat.ralaiseheno@esti.mg>
2025-02-16 22:53:05 +0300

    fix: ui tasklist

--- app/(protected)/task/components/TaskList.tsx
+++ app/(protected)/task/components/TaskList.tsx
@@ -1,9 +1,5 @@
 "use client"
 import React, { useEffect, useState } from 'react'
-import UrgentAndImportant from './UrgentAndImportant'
-import UrgentNotImportant from './UrgentNotImportant'
-import ImportantNotUrgent from './ImportantNotUrgent'
-import NotImportantAndNotUrgent from './NotImportantAndNotUrgent'
 import { fetchAllTaskByAssignAction } from '@/app/action/Task/fetchTaskById'
 import { useUser } from '@clerk/nextjs'
 import { PriorityTypeLevel, statusTypeTask } from '@/Models/Tache/$Type'
@@ -39,6 +35,32 @@ const TaskList = () => {
       setLoader(false)
     }
   }, [user]);
+  const taskInformation = [{
+    index: "1",
+    titre: "Urgent and Important",
+    BorderColor: "border-red-500",
+    BgColor: "bg-red-500",
+    Priority: "urgent_and_important"
+  }, {
+    index: "2",
+    titre: "Urgent and not Important",
+    BorderColor: "border-blue-600",
+    BgColor: "bg-blue-600",
+    Priority: "urgent_and__not_important"
+
+  }, {
+    index: "3",
+    titre: "Important Not Urgent",
+    BorderColor: "border-gray-600",
+    BgColor: "bg-gray-600",
+    Priority: "important_not_urgent"
+  }, {
+    index: "4",
+    titre: "Not Important Not Urgent ",
+    BorderColor: "border-green-800",
+    BgColor: "bg-green-800",
+    Priority: "not_important_not_urgent"
+  },]
   return (
     <div className='m-6'>
       {Loader ? (
@@ -49,10 +71,43 @@ const TaskList = () => {
         </>
       ) : (
         <div className="grid grid-cols-2 gap-4">
-          <UrgentAndImportant TasksList={Tasks} />
-          <UrgentNotImportant />
-          <ImportantNotUrgent />
-          <NotImportantAndNotUrgent />
+          {taskInformation.map((taskInfo, key) => {
+            const taskFilterPriority = Tasks.filter((task) => task.Priority === `${taskInformation[key].Priority}` && task.status === 'en_cours')
+            return (
+              <>
+                <div key={key} className='max-h-screen'>
+                  <article className={`rounded-xl border ${taskInfo.BorderColor} h-[450px] overflow-y-scroll`}>
+                    <div className={`flex ${taskInfo.BgColor} items-center p-5 rounded-t-xl gap-4`}>
+                      <div className='w-10 h-10 border-2 flex items-center justify-center rounded-full'>
+                        <span className='text-white font-bold'>{taskInfo.index}</span>
+                      </div>
+                      <div>
+                        <h3 className="text-lg font-medium text-white">{taskInfo.titre} ()</h3>
+                      </div>
+                    </div>
+                    <ul className="mt-4 p-4 space-y-2">
+                      {taskFilterPriority.map((tache, cle) => (
+                        <li key={cle} className='block h-full rounded-lg border border-gray-700 p-4 hover:border-pink-600"'>
+                          <div className='flex'>
+                            <input type="checkbox" className="checkbox checkbox-error" />
+                            <div className='ml-4'>
+                              <h3 className="">
+                                <strong className="font-bold">{tache.Title}(Deadline : {`${tache.Deadline.getDate()}/${tache.Deadline.getMonth()}/${tache.Deadline.getFullYear()}`})</strong>
+                              </h3>
+                              <p className="mt-1 text-xs font-medium">
+                                {tache.Description}
+                              </p>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </article>
+                </div>
+              </>
+            )
+          })}
+
         </div>
       )
       }
